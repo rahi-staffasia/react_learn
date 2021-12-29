e2c4783b45615ec54be6a0cb9d178cb1f64a0595
Rafat Rashid Rahi <rafatrahi@staffasia.org>
2021-12-29 18:03:41 +0600

    Started learning react from docs

---
+++ pages/posts/first-post.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+const FirstPost = () => (
+  <Layout>
+    <Head>
+      <title>First Post</title>
+    </Head>
+    <h1>First Post</h1>
+    <h2>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </h2>
+    <Image
+      src="/images/signature.png"
+      height={144}
+      width={144}
+      alt="Your name"
+    ></Image>
+    <p className="fun">This is nuts</p>
+  </Layout>
+);
+
+export default FirstPost;
