f58c52b916894d7bd370b3e7cbb1880aae2a1974
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2021-12-30 03:01:18 +0600

    Right now working on 'Polishing layout' page from nextjs.org

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,7 @@
 import styles from "./layout.module.css";
 
 export default function Layout({ children }) {
+  console.log("Printing childrens:   ");
+  console.log(children);
   return <div className={styles.container}>{children}</div>;
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,25 +3,29 @@ import Image from "next/image";
 import Head from "next/head";
 import Layout from "../../components/layout";
 
-const FirstPost = () => (
-  <Layout>
-    <Head>
-      <title>First Post</title>
-    </Head>
-    <h1>First Post</h1>
-    <h2>
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
-    </h2>
-    <Image
-      src="/images/signature.png"
-      height={144}
-      width={144}
-      alt="Your name"
-    ></Image>
-    <p className="fun">This is nuts</p>
-  </Layout>
-);
+const FirstPost = kita => {
+  console.log("Printing kita");
+  console.log(kita);
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <Image
+        src="/images/signature.png"
+        height={144}
+        width={144}
+        alt="Your name"
+      ></Image>
+      <p className="fun">This is nuts</p>
+    </Layout>
+  );
+};
 
 export default FirstPost;
